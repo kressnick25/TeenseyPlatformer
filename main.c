920fba41d08f1ac3da5dcdd34009d22fe2480763
nkress <kressnick25@gmail.com>
2018-09-24 00:40:04 +1000

    setup - main and makefile

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <cpu_speed.h>
+#include <macros.h>
+#include <graphics.h>
+#include <lcd.h>
+#include "lcd_model.h"
+
+int main ( void ){
+    return 0;
+}
