06d656d0d8f7d3178f0223a533a2f5a22478f772
nkress <kressnick25@gmail.com>
2018-10-12 18:47:43 +1000

    player movement and platform speed control

--- main.c
+++ main.c
@@ -3,22 +3,88 @@
 
 Sprite player;
 
+// Game states
+// TODO move these to single byte.
+bool gameOver = false;
+bool gamePause = false;
+bool gravity;
+bool playerCollision = true;
+bool treasureMove = true;
+float pot1Value; 
+
+
+void move_player(){
+    if ( BIT_VALUE(PIND, 0) && player.x < LCD_X - 5 && playerCollision)
+    {
+        if (player.dx < 0.5)
+        {
+            player.dx += 0.1;
+        }        
+    }
+    else if (BIT_VALUE(PINB, 1) && player.x > 0 && playerCollision)
+    {
+        if (player.dx > -0.5)
+        {
+            player.dx -= 0.1;
+        }
+    }
+    else if (BIT_VALUE(PIND, 1) && playerCollision)
+    {
+        player.y -= 1;
+        // Jumping with horizontal velocity moves further than
+        // falling with horizontal velocity
+        if (player.dy != 0)
+        { 
+            player.dy *= 1.5;
+        }
+    }
+    // move downwards, for testing only //TODO remove this
+    else if (BIT_VALUE(PINB, 7) && playerCollision)
+    {
+        player.y += 1;
+        // Jumping with horizontal velocity moves further than
+        // falling with horizontal velocity
+        if (player.dy != 0)
+        { 
+            player.dy *= 1.5;
+        }
+    }
+    else if (BIT_VALUE(PINF, 5)){ // TODO move this to seperate function (?)
+        // TODO debounce this
+        treasureMove = !treasureMove;
+    }
+    else 
+    {
+        if (player.dx > 0.0){
+            player.dx -= 0.1;
+        }
+        if (player.dx < 0.0){
+            player.dx += 0.1;
+        }
+    }
+}
 
 void init_buttons(){
-    CLEAR_BIT( DDRF, 6 );
+    CLEAR_BIT( DDRF, 5 ); // button 2 / right / sw3
+    CLEAR_BIT( DDRF, 6 ); // button 1/ left / sw2
+    CLEAR_BIT( DDRD, 1 ); // joystick up
+    CLEAR_BIT( DDRB, 7 ); // joystick down
+    CLEAR_BIT( DDRB, 1 ); // joystick left 
+    CLEAR_BIT( DDRD, 0 ); // joystick right
+    CLEAR_BIT( DDRB, 0 ); // joystick centre click
+    // TODO LED's
 }
 
 void setup_start(){
     set_clock_speed(CPU_8MHz);
     lcd_init(LCD_DEFAULT_CONTRAST);
     init_buttons();
-
+    adc_init(); //init pot1
+    // TODO change this to ascii per asci_font.h
     draw_string(8, 16, "Nicholas Kress", FG_COLOUR);
     draw_string(22, 24, "n9467688", FG_COLOUR);
     show_screen();
 }
-
-
 
 void setup_game(){
     create_platforms();
@@ -48,20 +114,28 @@ int main ( void ){
         _delay_ms(100);
     }
 
-
     setup_game();
 
     bool game_exit = false;
-    while(!game_exit){
-        bool game_exit = false;
-        while(!game_exit){
+    while(!game_exit)
+    {
+        while(!gameOver)
+        {   
+            while(!gamePause)
+            {
                 clear_screen();
-                sprite_step(&treasure);
+                check_pot();
+                move_player();
+                if (treasureMove) sprite_step(&treasure);
+                sprite_step(&player);
                 auto_move_platforms();
                 move_treasure();
                 draw_all();
                 show_screen();
+            }
+            //gamePauseScreen();    
         }
+        // gameOverScreen();
     }
 
 
--- platforms.h
+++ platforms.h
@@ -6,14 +6,61 @@
 
 Sprite Platforms[sizeOfPlatforms];
 // gs multiplier, alter to change gamespeed.
-float GS = 1;
+#define GS 1;
+#define MAX_PS 10
+float platformMultiplyer = 1;
+
+/*
+**	Initialize and enable ADC with pre-scaler 128.
+**
+**	Assuming CPU speed is 8MHz, sets the ADC clock to a frequency of
+**	8000000/128 = 62500Hz. Normal conversion takes 13 ADC clock cycles,
+**	or 0.000208 seconds. The first conversion will be slower, due to
+**	need to initialise ADC circuit.
+*/
+void adc_init() {
+	// ADC Enable and pre-scaler of 128: ref table 24-5 in datasheet
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+/*
+**	Do single conversion to read value of designated ADC pin combination.
+**
+**	Input:
+**	channel - A 6-bit value which specifies the ADC input channel and gain
+**			  selection. Refer table 24-4 in datasheet.
+**
+**	On TeensyPewPew, channel should be 0, 1, or 4.
+**	0 = Pot0
+**	1 = Pot1
+**	4 = Broken-out Pin F4.
+*/
+uint16_t adc_read(uint8_t channel) {
+	// Select AVcc voltage reference and pin combination.
+	// Low 5 bits of channel spec go in ADMUX(MUX4:0)
+	// 5th bit of channel spec goes in ADCSRB(MUX5).
+	ADMUX = (channel & ((1 << 5) - 1)) | (1 << REFS0);
+	ADCSRB = (channel & (1 << 5));
+
+	// Start single conversion by setting ADSC bit in ADCSRA
+	ADCSRA |= (1 << ADSC);
+
+	// Wait for ADSC bit to clear, signalling conversion complete.
+	while ( ADCSRA & (1 << ADSC) ) {}
+
+	// Result now available.
+	return ADC;
+}
 
 void create_platforms( void ) {
     //memset(Platforms, 0, sizeof Platforms);
     int initX = 0, initY = 9;
     int deltaY = 0;
     int c = 0;
-    float speed = 0.1 * GS;
+    platformMultiplyer = adc_read(1) / 80;
+    if (platformMultiplyer > MAX_PS) platformMultiplyer = MAX_PS;
+    //if (platformMultiplyer < 0.1) platformMultiplyer = 0;
+    float speed = 0.05 * platformMultiplyer;
     for (int i = 0; i < 4; i++)
     {
         int deltaX = 0;
@@ -60,4 +107,28 @@ void auto_move_platforms( void )
         //}
     }
 }
+
+void change_platform_speed( float speed )
+{
+    int c = 0;
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 7; j++){
+            Platforms[c].dx = 0.05 * speed;
+            c++;
+        }
+        speed = -speed;
+    }
+}
+
+void check_pot(){
+    float current_pot = adc_read(1) / 80;
+
+    if (platformMultiplyer != current_pot){
+        if (current_pot > MAX_PS) current_pot = MAX_PS;
+        change_platform_speed(current_pot);
+    }
+    platformMultiplyer = current_pot;
+}
+
+
 #endif 
